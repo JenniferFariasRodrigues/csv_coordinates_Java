e2f11c72d677b3604ac8e65a1f55daa9fea36a83
JenniferFariasRodrigues <jennifer.farias.rodrigues@gmail.com>
2024-07-10 16:28:46 -0300

    feat: creating a project

---
+++ csv_coordinates/src/main/java/com/csv_coordinates/csv_coordinates/CsvCoordinatesApplication.java
@@ -0,0 +1,13 @@
+package com.csv_coordinates.csv_coordinates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvCoordinatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvCoordinatesApplication.class, args);
+	}
+
+}
